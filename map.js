8ec1d86aa319f5dd8a50ed3de596e66cedca5ecb
rahmam <mohammed.rahman@cdk.com>
2017-07-17 11:24:53 +0530

    styles

--- map.js
+++ map.js
@@ -11,10 +11,10 @@ function initMap() {
 function setMarkers(map) {
 
     var stores = [
-        ['Chev Boston', 42.360082, -71.058880, 4],
-        ['Excellence Motor1', 42.359979, -71.056765, 5],
-        ['Excellence Motor2', 42.360487, -71.055201, 3],
-        ['Excellence Motor3', 42.360027, -71.056159, 1]
+        ['Chev Boston', 42.360082, -71.058880, 4, '9AM-6PM'],
+        ['Excellence Motor1', 42.359979, -71.056765, 5, '9:30AM-5:30PM'],
+        ['Excellence Motor2', 42.360487, -71.055201, 3, '9AM-5PM'],
+        ['Excellence Motor3', 42.360027, -71.056159, 1, '10AM-6PM']
     ];
 
     for (var i = 0; i < stores.length; i++) {
@@ -44,7 +44,14 @@ function prepareMarker(store, map) {
         zIndex: store[3]
     });
 
-    var content = store[0] + "<div><a target='_blank' href=''>Buy Now</a></div>";
+    var content =
+        "<div style='width:200px'>" +
+        "<h4> <img width='32' height='25' src='car.png'/> " +
+        store[0] + "</h4>" +
+        "<span>Working hours:" + store[4] + "</span>" +
+        "<hr>" +
+        "<h4><a target='_blank' href=''>Buy Now</a></h4>"+
+        "</div>";
 
     google.maps.event.addListener(marker, "click", function () {
         new google.maps.InfoWindow({ content: content }).open(map, marker);
